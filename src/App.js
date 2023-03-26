7bbe70026e479c887717410fa28579bada14f0b7
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-03-25 20:11:39 -0400

    Added background image to app; after some trial & error, leaving the tinting of the image for a later time...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles.modules.css'
 import './index.css'
+import background from './Images/background.JPEG';
 import React from 'react';
 import Header from './Components/Header';
 import Main from './Components/Main';
@@ -7,7 +8,27 @@ import Footer from './Components/Footer';
 
 function App() {
   return (
-    <>
+    <div
+      className='appBackground'
+      style={{
+        position: 'relative',
+        height: '100vh',
+      }}>
+    <div
+      className='backgroundImage'
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        zIndex: '-1',
+        opacity: '0.5',
+    }} />
     <header>
     <Header />
     </header>
@@ -17,7 +38,7 @@ function App() {
     <footer>
     <Footer />
     </footer>
-    </>
+    </div>
   );
 }
 
