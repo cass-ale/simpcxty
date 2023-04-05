c61c133e88b574698c5a998aebfee3c90dddfe4a
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-05 16:35:17 -0400

    Alt Image MouseOver

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import newRelease from "../Images/newestRelease.png";
-
+import Alt from '../Images/SaturdayAlt.png'
 
 function Hero() {
+const [imgSrc, setImgSrc] = useState(newRelease);
+const handleMouseOver = () => {
+    setImgSrc(Alt);
+}
+const handleMouseOut = () => {
+    setImgSrc(newRelease);
+}
     return (
         <div className="hero" id='hero'>
 
         <a href='https://unitedmasters.com/m/642b4f481e4cf0054bfba477' target="_blank" rel="noopener noreferrer">
         <section className="heroImage">
-            <img src={newRelease} alt="Newest Release From simpcxty Available Now!" />
+            <img onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} src={imgSrc} alt="Newest Release From simpcxty Available Now!" />
         </section>
         </a>
 
