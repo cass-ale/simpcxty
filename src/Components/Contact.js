1ac45dd5bf5a9ea0d0d04ca30886cae19a925f3c
Cass Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-06 13:53:02 -0400

    UX Boosts; scroll added to contact form and contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,15 +1,24 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import NotHomeHeader from './NotHomeHeader';
 import Footer from './Footer';
 import Form from './ContactForm'
 
 function Contact() {
+    const targetRef = useRef(null);
     const [showForm, setShowForm] = useState(false);
     const openForm = () => {
         setShowForm(true);
+        targetRef.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+          });
     };
     const closeForm = () => {
         setShowForm(false);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+          });
     }
     return (
         <>
@@ -20,7 +29,7 @@ function Contact() {
             <main>
             <div className="contactContainer">
 
-            <section id="conHeader">Connect With simpcxty</section>
+            <section id="conHeader" ref={targetRef}>Connect With simpcxty</section>
 
             <p>If You Are Interested In Working With <span style={{fontFamily: "Kristi", fontSize: "2.65rem"}}>simpcxty</span>, Or Eager To Talk With Him About His Work, Feel Free To Fill Out Our Contact Form, And Our Team Will Be In Touch With You Shortly After. All Inquiries Involving Music, Modeling, Custom Clothing, Or Interviews Can Be Submitted Here.<br /><br /> Please Note That This Inbox Is Managed By The CAPRI Media Administrative Team, And We Only Monitor Our Inbox During U.S. Business Hours (9:00-17:00) On Monday Through Friday. We Look Forward To Hearing From You!</p>
 
