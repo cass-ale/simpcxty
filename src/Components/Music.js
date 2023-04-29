08599a17f8e6a37bf9dc5413a6aad7005ea6eccb
Jace Arouet <jarouet@fb.com>
2023-04-28 21:50:39 -0400

    Styling

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Spotify } from "react-spotify-embed";
-import ten from "../Images/IMG_7377.JPG";
+import ten from "../Images/ten12cover.jpeg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faSpotify,
@@ -18,13 +18,16 @@ function Music() {
                 <Spotify link="https://open.spotify.com/track/7MGyNQLnIplRU7BXA4YfOV?si=9114d63457e64a5a" />
                 <Spotify link="https://open.spotify.com/track/09LSVcdvGkyi8ykv1xdFsO?si=a4ca2f182257408a" />
             </section>
+            <section className="firstAlbum">
             <section><h2>TEN12, An Album By</h2> <span id="simpSpan" style={{fontFamily: "Kristi"}}>simpcxty</span></section>
             <section className="ten12">
                 <img src={ten} alt="Alternative Cover for TEN12 by simpcxty" />
             </section>
                 <section className="tenLinks">
                     <a id="spotify" href="https://open.spotify.com/album/3cm4IUb0ydWvqbCEl9M136?si=z0SdGd15Sq2o7_R_6nxDUA" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faSpotify} size="5x" /></a>
-                    <a id="apple" href="https://music.apple.com/ng/album/ten12/1591993160" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faApple} size="5x" /></a>                </section>
+                    <a id="apple" href="https://music.apple.com/ng/album/ten12/1591993160" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faApple} size="5x" /></a>
+                </section>
+                </section>
         </div>
     )
 }
