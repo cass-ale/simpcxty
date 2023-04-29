faba255b4b27b11e56433aceda0a034ccfdd0736
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-29 12:18:48 -0400

    Added simp sessions

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -19,7 +19,9 @@ function Music() {
                 <Spotify link="https://open.spotify.com/track/09LSVcdvGkyi8ykv1xdFsO?si=a4ca2f182257408a" />
             </section>
             <section className="firstAlbum">
-            <section><h2>TEN12, An Album By</h2> <span id="simpSpan" style={{fontFamily: "Kristi"}}>simpcxty</span></section>
+            <section>
+                <h2>TEN12, An Album By</h2> <span id="simpSpan" style={{fontFamily: "Kristi"}}>simpcxty</span>
+            </section>
             <section className="ten12">
                 <img src={ten} alt="Alternative Cover for TEN12 by simpcxty" />
             </section>
@@ -27,7 +29,14 @@ function Music() {
                     <a id="spotify" href="https://open.spotify.com/album/3cm4IUb0ydWvqbCEl9M136?si=z0SdGd15Sq2o7_R_6nxDUA" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faSpotify} size="5x" /></a>
                     <a id="apple" href="https://music.apple.com/ng/album/ten12/1591993160" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faApple} size="5x" /></a>
                 </section>
+            </section>
+            <section className="simpSession">
+                <h2>LATEST SIMP SESSIONS</h2>
+                <section className="players">
+                <iframe id="yt" width="420" height="275" src="https://www.youtube-nocookie.com/embed/k8zgbGyUI2w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe id="yt" width="420" height="275" src="https://www.youtube-nocookie.com/embed/-PeQ2icIZ8U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </section>
+            </section>
         </div>
     )
 }
