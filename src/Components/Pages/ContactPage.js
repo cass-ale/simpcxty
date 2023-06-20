c3b4a2d63c6f54e53f32c2f2150483a7c444d7f1
Jace Arouet <jarouet@fb.com>
2023-06-20 17:34:49 -0400

    styling

--- src/Components/Pages/ContactPage.js
+++ src/Components/Pages/ContactPage.js
@@ -61,7 +61,7 @@ function Contact() {
             </section>
 
             <section className="contactContainer" ref={cardRef}>
-            <section id="conHeader" ref={targetRef}><span id="conHeader" style={{fontFamily: "SpecialElite", fontSize: '2.8rem'}}>Connect With</span> simpcxty</section>
+            <section id="conHeader" ref={targetRef}><span id="connect" style={{fontFamily: "SpecialElite", fontSize: '2.8rem'}}>Connect With</span> simpcxty</section>
 
             <p>If You Are Interested In Working With <span style={{fontFamily: "Kristi", fontSize: "2.65rem"}}>simpcxty</span>, Or Eager To Talk With Him About His Work, Feel Free To Fill Out Our Contact Form, And Our Team Will Be In Touch With You Shortly After. All Inquiries Involving Music, Modeling, Custom Clothing, Or Interviews Can Be Submitted Here.<br /><br /> Please Note That This Inbox Is Managed By The CAPRI Media Administrative Team, And We Only Monitor Our Inbox During U.S. Business Hours (9:00-17:00) On Monday Through Friday. We Look Forward To Hearing From You!</p>
 
