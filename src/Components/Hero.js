ea42fc18bba0296fa2733bbc465502e12bd9c148
Jace Arouet <jarouet@fb.com>
2023-06-20 18:38:46 -0400

    Revert "optimizing img loading"
    This reverts commit 84cbe179aad28069c233647656ac4796d11e6e04.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,8 +1,6 @@
 import React, {useState} from "react";
-import placeholder from '../Images/placehold.webp'
 import newRelease from "../Images/newestRelease.png";
 import Alt from '../Images/SaturdayAlt.png'
-import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function Hero() {
 const [imgSrc, setImgSrc] = useState(newRelease);
@@ -17,7 +15,7 @@ const handleMouseOut = () => {
 
         <a href='https://unitedmasters.com/m/642b4f481e4cf0054bfba477' target="_blank" rel="noopener noreferrer">
         <section className="heroImage">
-            <LazyLoadImage onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} width={560} height={560} placeholderSrc={placeholder} effect="blur" src={imgSrc} alt="Newest Release From simpcxty Available Now!" />
+            <img onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} src={imgSrc} alt="Newest Release From simpcxty Available Now!" />
         </section>
         </a>
 
